4bf5856e909d63e0fa8d7f7825431bef4fd31008
KingGamingYT <45918062+KingGamingYT@users.noreply.github.com>
2024-04-20 20:21:51 -0400

    Inline videos, Size toggle, and Code refactor
    With this update, the plugin has achieved everything I believe it should've. I'm so glad I had to help to make it a reality.
    Thank you greatly to NoSkillNoAndy for their invaluable help, slightly-less-novice-level skill, and late-night brainstorming sessions to make the functionality for this update possible, I never would've been able to do it without them. With that all being said, here are the additions.
    
    - Inline Video Playback: At long last, videos that are uploaded as part of a mosaic will no longer open up into the carousel interface to play, they will play in chat like any video posted standalone.
    
    - Size Toggle: The CSS for making images smaller (400px in width instead of the newer 500px) has been changed into a togglable setting in the plugin's new settings menu. It is enabled by default.
    
    - Code Refactor: The plugin's code has been overhauled to be less messy and more refined.
    
    Enjoy!

---
+++ NoMosaic.plugin.js
@@ -0,0 +1,100 @@
+/**
+ * @name NoMosaic
+ * @author Tanza, KingGamingYT, NoSkillPureAndy
+ * @description No more mosaic!
+ * @version 1.0.0
+ * @source https://github.com/KingGamingYT/discord-no-mosaic
+ */
+
+const { Data, Webpack, React, Patcher } = BdApi;
+
+const {FormSwitch} = Webpack.getByKeys('FormSwitch')
+const { createElement, useState } = React;
+
+const settings = {
+	cssSizeFix: {
+		name: "Reduce attachments' sizes",
+		note: "Makes attachments 400 pixels wide max like they were originally, rather than 500 pixels",
+        default: true
+	}
+};
+const shrinkImagesCSS = document.createElement("style");
+shrinkImagesCSS.innerHTML =
+`
+.lazyImg_b510a9,
+.lazyImgContainer_b0a5df {
+    width: auto !important;
+    height: min-content !important;
+}
+.visualMediaItemContainer__582ad {
+    max-width: 400px;
+}
+`;
+const borderRadiusCSS = document.createElement("style");
+borderRadiusCSS.innerHTML =
+`
+.oneByOneGridSingle__8c6ef,
+.imageDetailsAdded_sda9Fa .imageWrapper__178ee, /* ImageUtilities adds this */
+.visualMediaItemContainer__582ad {
+    border-radius: 2px !important;
+}
+`;
+
+module.exports = class NoMosaic {
+    constructor(meta) {
+
+    }
+    start() {
+        for (let key in settings) {
+            if (Data.load('NoMosaic', key) === undefined)
+                Data.save('NoMosaic', key, settings[key].default);
+        }
+
+        document.body.appendChild(borderRadiusCSS);
+        if (Data.load('NoMosaic', 'cssSizeFix'))
+            document.body.appendChild(shrinkImagesCSS);
+
+        const renderAttachmentsPatch = (self, args, ret) => {
+            if (!ret || !ret.props || !ret.props.items)
+                return;
+            for (let i = 0; i < ret.props.items.length; i++) {
+                if (!ret.props.items[i] || !ret.props.items[i].item || !ret.props.items[i].item.contentType)
+                    continue;
+                if (!ret.props.items[i].item.contentType.startsWith('image/'))
+                    delete ret.props.items[i].onClick;
+            }
+            return ret;
+        };
+
+        Patcher.instead('NoMosaic', Webpack.getByKeys('isGroupableMedia'), 'isGroupableMedia', () => {return false;});
+        Patcher.after('NoMosaic', Webpack.getAllByRegex(/renderAttachments/, {searchExports: true}).prototype, 'renderAttachments', renderAttachmentsPatch);
+    }
+
+    getSettingsPanel() {
+        return createElement(() => Object.keys(settings).map((key) => {
+	        const [state, setState] = useState(Data.load('NoMosaic', key));
+	        const { name, note } = settings[key];
+
+            return createElement(FormSwitch, {
+	        	children: name,
+	        	note: note,
+	        	value: state,
+	        	onChange: (v) => {
+	        		Data.save('NoMosaic', key, v);
+	        		setState(v);
+
+                    if (v)
+                        document.body.appendChild(shrinkImagesCSS);
+                    else
+                        document.body.removeChild(shrinkImagesCSS);
+	        	}
+	        });
+        }));
+	}
+
+    stop() {
+        BdApi.Patcher.unpatchAll('NoMosaic');
+        document.body.removeChild(shrinkImagesCSS);
+        document.body.removeChild(borderRadiusCSS);
+    }
+};
